1dfc1080b73dacb7499215c2029e2d51986ff93c
Ken Chen <kenchen@microsoft.com>
2021-04-15 15:53:11 +0800

    Add codestream sample (#93)
    * add codestream sample
    
    * fix package.json

---
+++ samples/codestream/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { WebPubSubServiceClient } = require('@azure/web-pubsub');
+
+const app = express();
+let endpoint = new WebPubSubServiceClient(process.argv[2], 'codestream');
+
+app.get('/negotiate', async (req, res) => {
+    let id = req.query.id || Math.random().toString(36).slice(2, 7);
+    let role = req.query.id
+      ? [`webpubsub.sendToGroup.${id}-control`, `webpubsub.joinLeaveGroup.${id}`]
+      : [`webpubsub.sendToGroup.${id}`, `webpubsub.joinLeaveGroup.${id}-control`];
+    let token = await endpoint.getAuthenticationToken({ claims: { role: role } });
+    res.json({
+      id: id,
+      url: token.url
+    });
+  });
+
+app.use(express.static('public'));
+app.listen(8080, () => console.log('app started'));
