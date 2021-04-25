0fef4556811697893eb24da3de2c6b8606e830b7
Liangying Wei <lianwei@microsoft.com>
2021-04-25 20:20:37 +0800

    Add csharp

---
+++ samples/csharp/pubsub/publisher/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using Azure.Messaging.WebPubSub;
+
+namespace publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2) {
+                Console.WriteLine("Usage: <connectionString> <hub> <message");
+                return;
+            }
+            var connectionString = args[0];
+            var hub = args[1];
+            var message = args[2];
+            
+            // Either generate the token or fetch it from server or fetch a temp one from the portal
+            var serviceClient = new WebPubSubServiceClient(connectionString, hub);
+            serviceClient.SendToAll(message);
+        }
+    }
+}
---
+++ samples/csharp/pubsub/subscriber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Azure.Messaging.WebPubSub;
+
+using Websocket.Client;
+
+namespace subscriber
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: subscriber <connectionString> <hub>");
+                return;
+            }
+            var connectionString = args[0];
+            var hub = args[1];
+
+            // Either generate the URL or fetch it from server or fetch a temp one from the portal
+            var serviceClient = new WebPubSubServiceClient(connectionString, hub);
+            var url = serviceClient.GetClientAccessUri();
+
+            using (var client = new WebsocketClient(url))
+            {
+                client.MessageReceived.Subscribe(msg => Console.WriteLine($"Message received: {msg}"));
+                await client.Start();
+                Console.WriteLine("Connected.");
+            }
+
+            Console.Read();
+        }
+    }
+}
