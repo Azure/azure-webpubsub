b00e5616ded326d85adf10c0bbbd4d3429eec1b2
Chenyang Liu <chenyl@microsoft.com>
2021-05-20 11:06:42 +0800

    Add logstream get started

--- samples/python/logstream/stream.py
+++ samples/python/logstream/stream.py
@@ -10,22 +10,24 @@ import json
 async def connect(url):
     async with websockets.connect(url, subprotocols=['json.webpubsub.azure.v1']) as ws:
         print('connected')
+        id = 1
         while True:
-            data = await ws.recv()
-            print(data)
+            data = input()
             payload = {
                 'type': 'sendToGroup',
                 'group': 'stream',
                 'dataType': 'text',
-                'data': str(data)
+                'data': str(data),
+                'ackId': id
             }
+            id = id + 1
             await ws.send(json.dumps(payload))
+            await ws.recv()
 
-res = requests.get('http://localhost:8080/negotiate').json()
+res = requests.get('http://localhost:8081/negotiate').json()
 
 try:
     asyncio.get_event_loop().run_until_complete(connect(res['url']))
 except KeyboardInterrupt:
     pass
-
 
