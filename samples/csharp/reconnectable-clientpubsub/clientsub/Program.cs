1ea6b405a1b76aa10c44304208409e4d5813aab1
Chenyang Liu <chenyl@microsoft.com>
2021-12-13 18:15:39 +0800

    New reconnectable client

--- samples/csharp/reconnectable-clientpubsub/clientsub/Program.cs
+++ samples/csharp/reconnectable-clientpubsub/clientsub/Program.cs
@@ -23,103 +23,54 @@ namespace clientsub
             //    Console.WriteLine("Usage: clientsub <connectionString> <hub>");
             //    return;
             //}
-            bool needAck = false;
-            if (args.Contains("ack"))
-            {
-                needAck = true;
-            }
+           
             var connectionString = "Endpoint=http://localhost:8080;AccessKey=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGH;Version=1.0;";
-            
+
+
             var hub = "signalrbench";
 
             // Either generate the URL or fetch it from server or fetch a temp one from the portal
             var serviceClient = new WebPubSubServiceClient(connectionString, hub);
-            var url = serviceClient.GenerateClientAccessUri(userId: "user1", roles: new string[] {"webpubsub.joinLeaveGroup.demogroup", "webpubsub.sendToGroup.demogroup"});
+            var ws = new WebPubSubServiceWebsocketClient(() => serviceClient.GenerateClientAccessUri(userId: "user1", roles: new string[] { "webpubsub.joinLeaveGroup.demogroup", "webpubsub.sendToGroup.demogroup" }),
+                "json.webpubsub.azure.v1");
 
-            var ws = new WebPubSubServiceWebsocketClient(url, "json.webpubsub.azure.v1");
-            ws.MessageReceived.Subscribe(msg => 
+            ws.MessageReceived.Subscribe(async msg => 
             {
-                var connected = JsonSerializer.Deserialize<Connected>(msg.Text);
-                if (connected != null && connected.@event == "connected")
+                Console.WriteLine($"Message received: {msg}");
+                var type = JsonSerializer.Deserialize<MessageType>(msg.Text);
+                if (type.type == "system")
                 {
-                    ws.ConnectionId = connected.connectionId;
-                    ws.ReconnectToken = connected.reconnectionToken;
-                }
-
-                var seqIdMessage = JsonSerializer.Deserialize<SequenceIdMessage>(msg.Text);
-                if (seqIdMessage != null && seqIdMessage.sequenceId != null)
-                {
-                    if (ws.IsDuplicate(seqIdMessage.sequenceId.Value))
+                    var connected = JsonSerializer.Deserialize<Connected>(msg.Text);
+                    if (connected.@event == "connected")
                     {
-                        Console.WriteLine($"Duplicated message received: sequence id: {seqIdMessage.sequenceId}");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Message received: {msg}");
-                    }
-
-                    if (needAck)
-                    {
-                        Console.WriteLine($"Ack: {seqIdMessage.sequenceId.Value}");
-                        _ = ws.SendAsync(JsonSerializer.Serialize(new
+                        await ws.SendAsync(1, JsonSerializer.Serialize(new
                         {
-                            type = "ack",
-                            sequenceId = seqIdMessage.sequenceId.Value,
+                            type = "joinGroup",
+                            group = "demogroup",
+                            ackId = 1
                         }));
                     }
                 }
-                else
+                else if(type.type == "ack")
                 {
-                    Console.WriteLine($"Message received: {msg}");
+                    var ack = JsonSerializer.Deserialize<AckMessage>(msg.Text);
+                    ws.HandleAck(new WebPubSubServiceWebsocketClient.AckMessage(ack.ackId, ack.success));
                 }
             });
 
             await ws.StartAsync();
-            Console.WriteLine("Connected.");
-            await ws.SendAsync(JsonSerializer.Serialize(new
-            {
-                type = "joinGroup",
-                group = "demogroup",
-                ackId = 1
-            }));
-
             
             while(true)
             {
-                //await Task.Delay(5000);
                 Console.ReadLine();
                 ws.Abort();
-                Console.ReadLine();
-                await ws.Reconnect();
             }
-            
-            //using (var client = new WebsocketClient(url, () =>
-            //{
-            //    var inner = new ClientWebSocket();
-            //    inner.Options.AddSubProtocol("json.webpubsub.azure.v1");
-            //    return inner;
-            //}))
-            //{
-            //    // Disable the auto disconnect and reconnect because the sample would like the client to stay online even no data comes in
-            //    client.ReconnectTimeout = null;
-            //    client.IsReconnectionEnabled = false;
-            //    client.DisconnectionHappened.Subscribe(disconnectionInfo =>
-            //    {
-            //        client.Reconnect()
-            //    });
-            //    client.MessageReceived.Subscribe(msg => Console.WriteLine($"Message received: {msg}"));
-            //    client.MessageReceived.Subscribe()
-            //    await client.Start();
-            //    Console.WriteLine("Connected.");
-            //    client.Send(JsonSerializer.Serialize(new
-            //    {
-            //        type = "joinGroup",
-            //        group = "demogroup",
-            //        ackId = 1
-            //    }));
-            //    Console.Read();
-            //}
         }
+    }
+
+    public class MessageType
+    {
+        public string type { get; set; }
     }
 
     public class Connected
@@ -129,6 +80,12 @@ namespace clientsub
         public string connectionId {  get; set; }
 
         public string reconnectionToken { get; set; }
+    }
+
+    public class AckMessage
+    {
+        public ulong ackId { get; set; }
+        public bool success { get; set; }
     }
 
     public class SequenceIdMessage
