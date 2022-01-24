4a43b2a5042342667a67e73ad0ff180fb6b19abb
Jialin Xin <jixin@microsoft.com>
2022-01-24 11:46:38 +0800

    resolve comments and sync with doc sample.

--- samples/csharp/chatapp/Startup.cs
+++ samples/csharp/chatapp/Startup.cs
@@ -1,9 +1,13 @@
+using System;
 using System.IO;
-
+using System.Runtime.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebPubSub.AspNetCore;
+using Microsoft.Azure.WebPubSub.Common;
 using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -42,7 +46,7 @@ namespace chatapp
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapWebPubSubHub<SampleChatHub>("/eventhandler/{event}");
+                endpoints.MapWebPubSubHub<SampleChatHub>("/eventhandler/{*path}");
 
                 endpoints.MapGet("/negotiate", async context =>
                 {
@@ -57,6 +61,73 @@ namespace chatapp
                     await context.Response.WriteAsync(serviceClient.GetClientAccessUri(userId: id).AbsoluteUri);
                 });
             });
+        }
+
+        private sealed class SampleChatHub : WebPubSubHub
+        {
+            private readonly WebPubSubServiceClient<SampleChatHub> _serviceClient;
+
+            public SampleChatHub(WebPubSubServiceClient<SampleChatHub> serviceClient)
+            {
+                _serviceClient = serviceClient;
+            }
+
+            public override ValueTask<ConnectEventResponse> OnConnectAsync(ConnectEventRequest request, CancellationToken cancellationToken)
+            {
+                // not register event will never be triggered.
+                return base.OnConnectAsync(request, cancellationToken);
+            }
+
+            public override async Task OnConnectedAsync(ConnectedEventRequest request)
+            {
+                await _serviceClient.SendToAllAsync($"[SYSTEM] {request.ConnectionContext.UserId} joined.");
+            }
+
+            public override async ValueTask<UserEventResponse> OnMessageReceivedAsync(UserEventRequest request, CancellationToken cancellationToken)
+            {
+                await _serviceClient.SendToAllAsync($"[{request.ConnectionContext.UserId}] {request.Data}");
+
+                // Advanced sample to show usage of connection state
+                // Retrieve counter from ConnectionContext.ConnectionStates or init if not exists.
+                var states = new CounterState(1);
+                var idle = 0.0;
+                if (request.ConnectionContext.ConnectionStates.TryGetValue(nameof(CounterState), out var counterValue))
+                {
+                    states = counterValue.ToObjectFromJson<CounterState>();
+                    idle = (DateTime.Now - states.Timestamp).TotalSeconds;
+                    states.Update();
+                }
+                // Build response .
+                var response = request.CreateResponse($"[SYSTEM] ack, idle: {idle}s, connection message counter: {states.Counter}", WebPubSubDataType.Json);
+                response.SetState(nameof(CounterState), BinaryData.FromObjectAsJson(states));
+
+                return response;
+            }
+
+            // A simple class of user defined states to count message and time.
+            [DataContract]
+            private sealed class CounterState
+            {
+                [DataMember(Name = "timestamp")]
+                public DateTime Timestamp { get; set; }
+                [DataMember(Name = "counter")]
+                public int Counter { get; set; }
+
+                public CounterState()
+                { }
+
+                public CounterState(int counter)
+                {
+                    Counter = counter;
+                    Timestamp = DateTime.Now;
+                }
+
+                public void Update()
+                {
+                    Timestamp = DateTime.Now;
+                    Counter++;
+                }
+            }
         }
     }
 }
