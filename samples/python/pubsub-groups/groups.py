87dfc9d3fea40c1dc002273ec2d8e80787e66e41
Chris Lovett <clovett@microsoft.com>
2022-02-18 18:19:28 -0800

    fix: add some retry logic needed to make the program work indefinitely.  Azure web pub sub sockets do eventually timeout afer a couple days.

--- samples/python/pubsub-groups/groups.py
+++ samples/python/pubsub-groups/groups.py
@@ -3,6 +3,7 @@
 
 import json
 import asyncio
+from torch import true_divide
 import websockets
 from azure.messaging.webpubsubservice import WebPubSubServiceClient
 import queue
@@ -26,26 +27,31 @@ class WebPubSubGroup:
         self.listeners += [handler]
 
     async def connect(self):
-        self.client = WebPubSubServiceClient.from_connection_string(
-            connection_string=self.webpubsub_constr, hub=self.hub_name)
-        self.closed = False
-        token = self.client.get_client_access_token(
-            user_id=self.user_name,
-            roles=[f"webpubsub.joinLeaveGroup.{self.group_name}",
-                   f"webpubsub.sendToGroup.{self.group_name}"])
-        uri = token['url']
-        self.web_socket = await websockets.connect(
-            uri, subprotocols=['json.webpubsub.azure.v1'])
-        response = await self._send_receive({
-            "type": "joinGroup",
-            "ackId": self.ack_id,
-            "group": self.group_name})
-        self.ack_id += 1
-        # now we should have the connection id and an idea of success
-        if "event" in response and response["event"] == "connected":
-            self.connection_id = response["connectionId"]
-        print("WebSocket connected")
-        return
+        try:
+            self.client = WebPubSubServiceClient.from_connection_string(
+                connection_string=self.webpubsub_constr, hub=self.hub_name)
+            self.closed = False
+            token = self.client.get_client_access_token(
+                user_id=self.user_name,
+                roles=[f"webpubsub.joinLeaveGroup.{self.group_name}",
+                    f"webpubsub.sendToGroup.{self.group_name}"])
+            uri = token['url']
+            self.web_socket = await websockets.connect(
+                uri, subprotocols=['json.webpubsub.azure.v1'])
+            response = await self._send_receive({
+                "type": "joinGroup",
+                "ackId": self.ack_id,
+                "group": self.group_name})
+            self.ack_id += 1
+            # now we should have the connection id and an idea of success
+            if "event" in response and response["event"] == "connected":
+                self.connection_id = response["connectionId"]
+            print("WebSocket connected")
+            return True
+        except Exception as e:
+            print("### web socket connect failed: " + e)
+            self.web_socket = None
+            return False
 
     def send(self, msg):
         groupMessage = {
@@ -62,9 +68,20 @@ class WebPubSubGroup:
         while not self.closed:
             if not self.send_queue.empty():
                 message = self.send_queue.get()
-                if message:
-                    data = json.dumps(message)
-                    await self.web_socket.send(data)
+                if self.web_socket:
+                    if message:
+                        data = json.dumps(message)
+                        try:
+                            await self.web_socket.send(data)
+                        except Exception as e:
+                            # websocket has been closed, this can happen if the program is
+                            # running for days and Azure needs to move the service to another
+                            # VM.
+                            self.web_socket = None
+                            await asyncio.sleep(1)
+                            print("### websocket send error: ", e)
+                else:
+                    await self.connect()  # auto-reconnect!
             else:
                 await asyncio.sleep(0.1)
 
@@ -76,10 +93,19 @@ class WebPubSubGroup:
 
     async def listen(self):
         print("Listening for messages from WebSocket...")
-        async for message in self.web_socket:
-            self._handle_message(message)
-            if not self.client:
-                break
+        while not self.closed:
+            try:
+                if not self.web_socket:
+                    await self.connect()  # auto-reconnect!
+                async for message in self.web_socket:
+                    self._handle_message(message)
+            except Exception as e:
+                # websocket has been closed, this can happen if the program is
+                # running for days and Azure needs to move the service to another
+                # VM.
+                self.web_socket = None
+                await asyncio.sleep(1)
+                print("### websocket receive error: ", e)
         print("Stopped listening to WebSocket.")
 
     def _handle_message(self, data):
@@ -92,6 +118,8 @@ class WebPubSubGroup:
                     h(user, message)
 
     async def _send_receive(self, msg):
+        if not self.web_socket:
+            return {}
         data = json.dumps(msg)
         await self.web_socket.send(data)
         response = await self.web_socket.recv()
