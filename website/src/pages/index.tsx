fc822bf578a164b832f2c88c91185c050b99eb8b
wanl <wanl@microsoft.com>
2022-04-11 11:55:56 +0800

    add navbar

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
+import React from 'react'
+import { initializeIcons } from '@fluentui/font-icons-mdl2';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import NavBar from '@site/src/components/NavBar'
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-      </div>
-    </header>
-  );
-}
+initializeIcons()
 
 export default function Home(): JSX.Element {
   return (
-    <Layout>
-      <HomepageHeader />
-      <main>
-      </main>
-    </Layout>
-  );
+    <div>
+      <NavBar></NavBar>
+    </div>
+  )
 }
