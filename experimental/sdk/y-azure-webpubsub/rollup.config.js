40b5772b76c2ff924719ab8dd7d6d788de223c0d
stdrickforce <stdrickforce@gmail.com>
2022-04-18 13:35:35 +0800

    create CommonJs file for browser usage

---
+++ experimental/sdk/y-azure-webpubsub/rollup.config.js
@@ -0,0 +1,20 @@
+export default {
+  input: "./dist/index.js",
+  external: (id) => /^(lib0|yjs|y-protocols)/.test(id),
+  output: [
+    {
+      name: "y-azure-webpubsub",
+      file: "dist/y-azure-webpubsub.cjs",
+      format: "cjs",
+      sourcemap: true,
+      paths: (path) => {
+        if (/^lib0\//.test(path)) {
+          return `lib0/dist${path.slice(4)}.cjs`;
+        } else if (/^y-protocols\//.test(path)) {
+          return `y-protocols/dist${path.slice(11)}.cjs`;
+        }
+        return path;
+      },
+    },
+  ],
+};
