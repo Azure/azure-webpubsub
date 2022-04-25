aca15bf4d46ecd3aa41e56df10ce781eef14c7aa
Wanpeng Li <wanl@microsoft.com>
2022-04-25 11:30:34 +0800

    finetune mobile styles (#392)

--- website/src/theme/Navbar/index.tsx
+++ website/src/theme/Navbar/index.tsx
@@ -7,52 +7,83 @@ import * as styles from './styles.module'
 
 initializeIcons()
 
-export default function NavBar(): JSX.Element {
-  const isWide = IsWideDevice()
+function Brand(): JSX.Element {
+  return (
+    <Stack.Item grow>
+      <Stack horizontal tokens={styles.leftNavTokens} styles={styles.leftNav}>
+        <Stack.Item>
+          <Stack>
+            <ImageIcon
+              className={styles.classNames.logo}
+              aria-label="Locked"
+              imageProps={{
+                src: '/img/logo.png',
+                alt: 'logo',
+                className: css(styles.classNames.image, styles.classNames.logoImage),
+              }}
+            />
+          </Stack>
+        </Stack.Item>
+        <Stack.Item>
+          <Label styles={styles.title}>Web PubSub Service Demo Platform</Label>
+        </Stack.Item>
+      </Stack>
+    </Stack.Item>
+  )
+}
+
+function Search(): JSX.Element {
+  return (
+    <Stack.Item grow>
+      <SearchBox placeholder="Search demos by keyword. e.g. chat" styles={styles.searchBox} />
+    </Stack.Item>
+  )
+}
+
+function Contact(): JSX.Element {
+  return (
+    <Stack.Item grow>
+      <Stack horizontal horizontalAlign="end" tokens={styles.rightNavTokens} styles={styles.rightNav}>
+        <Stack.Item>
+          <FontIcon aria-label="Chat" iconName="ChatInviteFriend" className={styles.classNames.navBarIcon} />
+        </Stack.Item>
+        <Stack.Item>
+          <FontIcon aria-label="Question" iconName="StatusCircleQuestionMark" className={styles.classNames.navBarIcon} />
+        </Stack.Item>
+      </Stack>
+    </Stack.Item>
+  )
+}
+
+function NavBarDesktop(): JSX.Element {
   return (
     <div className="navbar" style={styles.root}>
       <Stack horizontal horizontalAlign="space-between" styles={styles.navBar} tokens={styles.stackTokens}>
-        {/* left */}
-        <Stack.Item grow>
-          <Stack horizontal tokens={styles.leftNavTokens} styles={styles.leftNav}>
-            <Stack.Item>
-              <Stack>
-                <ImageIcon
-                  className={styles.classNames.logo}
-                  aria-label="Locked"
-                  imageProps={{
-                    src: '/img/logo.png',
-                    alt: 'logo',
-                    className: css(styles.classNames.image, styles.classNames.logoImage),
-                  }}
-                />
-              </Stack>
-            </Stack.Item>
-            {isWide && (
-              <Stack.Item>
-                <Label styles={styles.title}>Web PubSub Service Demo Platform</Label>
-              </Stack.Item>
-            )}
-          </Stack>
-        </Stack.Item>
-
-        {/* center */}
-        <Stack.Item grow>
-          <SearchBox placeholder="Search demos by keyword. e.g. chat" styles={styles.searchBox} />
-        </Stack.Item>
-
-        {/* right */}
-        <Stack.Item grow>
-          <Stack horizontal horizontalAlign="end" tokens={styles.rightNavTokens} styles={styles.rightNav}>
-            <Stack.Item>
-              <FontIcon aria-label="Chat" iconName="ChatInviteFriend" className={styles.classNames.navBarIcon} />
-            </Stack.Item>
-            <Stack.Item>
-              <FontIcon aria-label="Question" iconName="StatusCircleQuestionMark" className={styles.classNames.navBarIcon} />
-            </Stack.Item>
-          </Stack>
-        </Stack.Item>
+        <Brand></Brand>
+        <Search></Search>
+        <Contact></Contact>
       </Stack>
     </div>
   )
+}
+
+function NavBarMobile(): JSX.Element {
+  return (
+    <div className="navbar" style={styles.root}>
+      <Stack>
+        <Stack horizontal horizontalAlign="space-between" styles={styles.navBar} tokens={styles.stackTokens}>
+          <Brand></Brand>
+          <Contact></Contact>
+        </Stack>
+        <Stack horizontal horizontalAlign="space-between" styles={styles.navBar} tokens={styles.stackTokens}>
+          <Search></Search>
+        </Stack>
+      </Stack>
+    </div>
+  )
+}
+
+export default function NavBar(): JSX.Element {
+  const isWide = IsWideDevice()
+  return isWide ? NavBarDesktop() : NavBarMobile()
 }
