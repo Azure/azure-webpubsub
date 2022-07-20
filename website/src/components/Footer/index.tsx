831487b2e36fd825c15533bb7d7eb360ad41c29c
JialinXin <jixin@microsoft.com>
2022-07-20 13:29:41 +0800

    update privacy link to fwlink refer to guidance.

--- website/src/components/Footer/index.tsx
+++ website/src/components/Footer/index.tsx
@@ -15,7 +15,7 @@ export default function Footer(): JSX.Element {
     return (
         <Stack className={styles.footer} horizontal horizontalAlign='end' tokens={tokens}>
             {visible && <Link className={styles.item} onClick={() => {window.siteConsent.manageConsent()}}>Manage Cookies</Link>}
-            <Link className={styles.item} href="https://privacy.microsoft.com/privacystatement" target="_blank">Privacy</Link>
+            <Link className={styles.item} href="https://go.microsoft.com/fwlink/?LinkId=521839" target="_blank">Privacy</Link>
             <span className={styles.item}>© Microsoft 2022</span>
         </Stack>
     )
