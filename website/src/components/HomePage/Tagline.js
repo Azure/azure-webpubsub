8518a5cfabbf49b104f1722e0651fe7688f655a4
Kevin Guo <32696592+Kaifei-Guo@users.noreply.github.com>
2022-08-24 13:24:16 +0800

    Configured TailwindCSS and added a few missing React components

---
+++ website/src/components/HomePage/Tagline.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Tagline() {
+  return (
+    <section className="bg-gray-50 px-4 py-28">
+      <h1 className="py-3 text-center text-4xl font-bold">Azure Web PubSub</h1>
+      <p className="text-center">
+        Easily add real-time capabilities to your apps using your preferred tech
+        stack.
+      </p>
+      <p className="text-center font-semibold">
+        Reliable. Secure. Highly scalable.
+      </p>
+    </section>
+  );
+}
+
+export default Tagline;
