5d9ed34a30e019d3d8b2a15b0205e450adc0c8bd
Kevin Guo <32696592+Kaifei-Guo@users.noreply.github.com>
2022-09-20 17:36:32 +0800

    Replaced placeholder text with real content and picture

--- website/src/DataDemos.js
+++ website/src/DataDemos.js
@@ -1,88 +1,70 @@
 export let DataDemos = [
   {
     id: 1,
-    title: "Simple chat app",
-    description:
-      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam",
+    title: 'Simple chat app',
+    description: 'A simple real-time chat app demonstrating the use of JavaScript server SDK',
     languages: [
-      { language: "C#", percent: "27" },
-      { language: "Java", percent: "52" },
-      { language: "HTML", percent: "21" },
+      // { language: 'JavaScript', percent: '' }
     ],
-    detailURL: "demos/simple_chat_app",
-    thumbnailURL: "img/thumbnails/code.png",
-
-    githubRepo:
-      "https://github.com/Azure/azure-webpubsub/tree/main/samples/javascript/chatapp",
+    detailURL: 'demos/simple_chat_app',
+    thumbnailURL: 'img/thumbnails/chat_abstract.jpeg',
+    githubRepo: 'https://github.com/Azure/azure-webpubsub/tree/main/samples/javascript/chatapp',
   },
   {
     id: 2,
-    title: "Collaborative whiteboard",
-    description:
-      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam",
+    title: 'Collaborative whiteboard',
+    description: 'A whiteboard app demonstrating how to build real-time collaborative apps using Azure Web PubSub and Node.js',
     languages: [
-      { language: "Python", percent: "15" },
-      { language: "JavaScript", percent: "55" },
-      { language: "HTML", percent: "30" },
+      // { language: 'JavaScript', percent: '' }
     ],
-    detailURL: "demos/collaborative_whiteboard",
-    thumbnailURL: "img/thumbnails/navigation.png",
-    githubRepo:
-      "https://github.com/Azure/azure-webpubsub/tree/main/samples/javascript/whiteboard",
+    detailURL: 'demos/collaborative_whiteboard',
+    thumbnailURL: 'img/thumbnails/whiteboard.jpeg',
+    githubRepo: 'https://github.com/Azure/azure-webpubsub/tree/main/samples/javascript/whiteboard',
   },
   {
     id: 3,
-    title: "Metaverse",
-    description:
-      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam",
+    title: 'Metaverse',
+    description: 'An app demonstrating how Azure Web PubSub can be used to enable multi-player experience in Metaverse (coming soon)',
     languages: [
-      { language: "C#", percent: "27" },
-      { language: "Java", percent: "73" },
+      // { language: 'C#', percent: '27' },
     ],
-    detailURL: "demos/metaverse",
-    thumbnailURL: "img/thumbnails/dashboard.png",
-    githubRepo: "",
+    detailURL: 'demos/metaverse',
+    thumbnailURL: 'img/thumbnails/metaverse.jpeg',
+    githubRepo: '',
   },
   {
     id: 4,
-    title: "Code stream",
-    description:
-      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam",
+    title: 'Code stream',
+    description: 'Real-time collaborative code editor',
     languages: [
-      { language: "C#", percent: "27" },
-      { language: "Java", percent: "73" },
+      // { language: 'C#', percent: '27' },
     ],
-    detailURL: "demos/code_stream",
-    thumbnailURL: "img/thumbnails/stock.png",
-    githubRepo:
-      "https://github.com/Azure/azure-webpubsub/tree/main/samples/javascript/codestream",
+    detailURL: 'demos/code_stream',
+    thumbnailURL: 'img/thumbnails/code.png',
+    githubRepo: 'https://github.com/Azure/azure-webpubsub/tree/main/samples/javascript/codestream',
   },
   {
     id: 5,
-    title: "Chatr",
-    description:
-      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam",
+    title: 'Chatr',
+    description: 'This demo is developed by Ben Coleman using Azure Web PubSub service, Azure Static Web Apps, and deploy using Azure Bicep.',
     languages: [
-      { language: "C#", percent: "27" },
-      { language: "Java", percent: "73" },
+      // { language: 'C#', percent: '27' },
     ],
-    detailURL: "demos/chatr",
-    thumbnailURL: "img/thumbnails/navigation.png",
-    githubRepo:
-      "https://github.com/Azure/azure-webpubsub/tree/main/samples/javascript/chatr",
+    detailURL: 'demos/chatr',
+    thumbnailURL: 'img/thumbnails/chat_closeup.jpeg',
+    githubRepo: 'https://github.com/Azure/azure-webpubsub/tree/main/samples/javascript/chatr',
   },
   {
     id: 6,
-    title: "Real-time scoreboard",
-    description:
-      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam",
+    title: 'Real-time scoreboard',
+    description: 'This app demonstrates how to push data from server to connected clients using Azure Web PubSub',
     languages: [
-      { language: "C#", percent: "27" },
-      { language: "Java", percent: "73" },
+      // { language: 'C#', percent: '27' },
     ],
-    detailURL: "demos/realtime_scoreboard",
-    thumbnailURL: "img/thumbnails/navigation.png",
-    githubRepo:
-      "https://github.com/Azure/azure-webpubsub/tree/main/samples/javascript/scoreboard",
+    detailURL: 'demos/realtime_scoreboard',
+    thumbnailURL: 'img/thumbnails/scoreboard.jpeg',
+    githubRepo: 'https://github.com/Azure/azure-webpubsub/tree/main/samples/javascript/scoreboard',
+    deployLink:
+      'https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Flivedemopackages.blob.core.windows.net%2Ftemplate%2Fscoreboard-deploy.json',
   },
-];
+]
--- website/src/pages/contact_us.js
+++ website/src/pages/contact_us.js
@@ -1,8 +1,8 @@
-import React from "react";
-import Layout from "@theme/Layout";
-import HyperLink from "@site/src/components/Common/HyperLink";
+import React from 'react'
+import Layout from '@theme/Layout'
+import HyperLink from '@site/src/components/Common/HyperLink'
 
-import GitHub from "@site/static/logos/github_logo.svg";
+import GitHub from '@site/static/logos/github_logo.svg'
 
 function ContactUs() {
   return (
@@ -12,52 +12,39 @@ function ContactUs() {
           <section className="mt-10">
             <h1 className="py-3 text-5xl font-bold">Where to Get Support?</h1>
             <p className="text-gray-800">
-              Quis nostrum exercitationem ullam corporis suscipit laboriosam,
-              nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure
-              reprehenderit qui in ea voluptate velit esse quam nihil molestiae
-              consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla
-              pariatu{" "}
+              If you already have a Web PubSub resource created on Azure portal, you can create a support ticket. Alternatively, you can create an issue on the
+              public GitHub issue. Our team monitors the issues very closely and tries to resolve the issue as soon as we can.
             </p>
           </section>
 
           <section>
             <h2 className="mt-10 py-1 text-3xl font-bold">Documentation</h2>
             <p className="text-gray-800">
-              You can visit the documentation of Azure Web PubSub service
-              from&nbsp;
-              <HyperLink to="https://docs.microsoft.com/en-us/azure/azure-web-pubsub/overview" />
-              .
+              You can visit the documentation of Azure Web PubSub service from&nbsp;
+              <HyperLink to="https://docs.microsoft.com/en-us/azure/azure-web-pubsub/overview" />.
             </p>
           </section>
 
           <section>
-            <h2 className="mt-10 py-1 text-3xl font-bold">
-              Reach out to the product team
-            </h2>
+            <h2 className="mt-10 py-1 text-3xl font-bold">Reach out to the product team</h2>
             <h3 className="font-bold">
               Create a GitHub
               <GitHub className="icon_inline" />
               issue
             </h3>
             <p className="text-gray-800">
-              The product team monitors GitHub issues on a daily basis. It’s the
-              best way to reach out the team and ... Create a GitHub issue&nbsp;
-              <HyperLink to="https://docs.microsoft.com/en-us/azure/azure-web-pubsub/overview" />
-              .
+              The product team monitors GitHub issues on a daily basis. It’s the best way to reach out the team and ... Create a GitHub issue&nbsp;
+              <HyperLink to="https://github.com/Azure/azure-webpubsub" />.
             </p>
           </section>
 
           <section>
             <h2 className="mt-10 py-1 text-3xl font-bold">Suggest a demo</h2>
             <p className="text-gray-800">
-              Have a demo you’d like to see on this site. Share your idea with
-              us and creating a GitHub issue&nbsp;
-              <HyperLink to="https://docs.microsoft.com/en-us/azure/azure-web-pubsub/overview" />
-              .
+              Have a demo you’d like to see on this site. Share your idea with us and creating a GitHub issue&nbsp;
+              <HyperLink to="https://github.com/Azure/azure-webpubsub" />.
             </p>
-            <h3 className="pt-2 font-bold">
-              Demos we are currently working on
-            </h3>
+            <h3 className="pt-2 font-bold">Demos we are currently working on</h3>
             <ul className="list-inside list-disc">
               <li>Collaborative sales order processing app</li>
               <li>Truck tracking app</li>
@@ -66,7 +53,7 @@ function ContactUs() {
         </div>
       </div>
     </Layout>
-  );
+  )
 }
 
-export default ContactUs;
+export default ContactUs
