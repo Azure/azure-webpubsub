269b403fe92eaf191907e5af9e99774435530bc4
Jialin Xin <jixin@microsoft.com>
2022-12-09 19:17:15 +0800

    add wps function python sample.

---
+++ samples/functions/python/notifications/index/__init__.py
@@ -0,0 +1,7 @@
+import os
+
+import azure.functions as func
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    f = open(os.path.dirname(os.path.realpath(__file__)) + '/../index.html')
+    return func.HttpResponse(f.read(), mimetype='text/html')
