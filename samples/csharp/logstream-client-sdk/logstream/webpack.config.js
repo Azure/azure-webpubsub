6e5d38048af5f4fc4627a1a30a3811deb07206ad
Chenyang Liu <chenyl@microsoft.com>
2023-01-29 17:12:14 +0800

    Add log stream client sample

---
+++ samples/csharp/logstream-client-sdk/logstream/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].[chunkhash].js",
+    path: path.resolve(__dirname, "wwwroot"),
+    publicPath: "/",
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+    fallback: {
+      "events": require.resolve("events/")
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    })
+  ]
+};
