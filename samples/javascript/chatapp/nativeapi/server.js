706bddfe7877869ece6d235167e7465da6e5a261
Liangying.Wei <lianwei@microsoft.com>
2023-05-24 13:42:03 +0800

    Change to use yarn workspace to manage all the javascript projects (#497)

--- samples/javascript/chatapp/nativeapi/server.js
+++ samples/javascript/chatapp/nativeapi/server.js
@@ -1,45 +1,45 @@
-const express = require('express');
-const { WebPubSubServiceClient } = require('@azure/web-pubsub');
-const { WebPubSubEventHandler } = require('@azure/web-pubsub-express');
+const express = require("express");
+const { WebPubSubServiceClient } = require("@azure/web-pubsub");
+const { WebPubSubEventHandler } = require("@azure/web-pubsub-express");
 
 const app = express();
-const hubName = 'sample_chat';
+const hubName = "sample_chat";
 const port = 8080;
 
 let connectionString = process.argv[2] || process.env.WebPubSubConnectionString;
 let serviceClient = new WebPubSubServiceClient(connectionString, hubName);
 let handler = new WebPubSubEventHandler(hubName, {
-  path: '/eventhandler',
-  onConnected: async req => {
+  path: "/eventhandler",
+  onConnected: async (req) => {
     console.log(`${req.context.userId} connected`);
     await serviceClient.sendToAll({
       type: "system",
-      message: `${req.context.userId} joined`
+      message: `${req.context.userId} joined`,
     });
   },
   handleUserEvent: async (req, res) => {
-    if (req.context.eventName === 'message') {
+    if (req.context.eventName === "message") {
       await serviceClient.sendToAll({
         from: req.context.userId,
-        message: req.data
+        message: req.data,
       });
     }
     res.success();
-  }
+  },
 });
 
 app.use(handler.getMiddleware());
-app.get('/negotiate', async (req, res) => {
+app.get("/negotiate", async (req, res) => {
   let id = req.query.id;
   if (!id) {
-    res.status(400).send('missing user id');
+    res.status(400).send("missing user id");
     return;
   }
   let token = await serviceClient.getClientAccessToken({ userId: id });
   res.json({
-    url: token.url
+    url: token.url,
   });
 });
 
-app.use(express.static('public'));
-app.listen(port, () => console.log(`Event handler listening at http://localhost:${port}${handler.path}`));
+app.use(express.static("public"));
+module.exports = app.listen(port, () => console.log(`Event handler listening at http://localhost:${port}${handler.path}`));
