2f1eb75716989c78f27a8af56ee58c2848fbe615
xingsy97 <87063252+xingsy97@users.noreply.github.com>
2023-05-29 12:12:28 +0800

    Add interface for Socket.IO Server SDK (#498)

---
+++ experimental/sdk/webpubsub-socketio-extension/src/EIO/components/web-pubsub-transport.ts
@@ -0,0 +1,18 @@
+import { Packet } from "engine.io-parser";
+import { Transport } from "engine.io";
+
+/**
+ * A class inherited from Engine.IO Transport class, it acts the same role with `Polling` Transport and `WebSocket` Transport.
+ * Compared with the other two transports, this transport always does batch receive and optional batch send (default true).
+ *
+ * TODO: Batch receive is implemented in web-pubsub-translator. It should be moved here later.
+ **/
+export class WebPubSubTransport extends Transport {
+  override supportsFraming = () => false;
+
+  override name = () => "webpubsub";
+
+  public override async send(packets: Packet[]) {}
+
+  public override doClose(fn) {}
+}
