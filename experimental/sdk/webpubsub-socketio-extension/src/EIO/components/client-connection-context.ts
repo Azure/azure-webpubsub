4423222213c99c186c0475fad886ef6bb72a2ccc
xingsy97 <87063252+xingsy97@users.noreply.github.com>
2023-05-29 18:02:37 +0800

    update

--- experimental/sdk/webpubsub-socketio-extension/src/EIO/components/client-connection-context.ts
+++ experimental/sdk/webpubsub-socketio-extension/src/EIO/components/client-connection-context.ts
@@ -1,6 +1,30 @@
+import { debugModule } from "../../common/utils";
+import { HubSendToConnectionOptions, WebPubSubServiceClient } from "@azure/web-pubsub";
+import EventEmitter from "events";
+
+const debug = debugModule("wps-sio-ext:EIO:ClientConnectionContext");
+
 /**
  * A logical concept that stands for an Engine.IO client connection, every connection has a unique `connectionId`.
  * It maps Engine.IO Transport behaviours to Azure Web PubSub service REST API calls.
  */
-class ClientConnectionContext {
+export class ClientConnectionContext extends EventEmitter {
+  public serviceClient: WebPubSubServiceClient;
+  public connectionId: string;
+
+  constructor(serviceClient: WebPubSubServiceClient, connectionId: string) {
+    super();
+    this.serviceClient = serviceClient;
+    this.connectionId = connectionId;
+  }
+
+  async send(packet: any, cb?: (err?: Error) => void) {
+    debug(`send packet ${packet}, type = ${typeof packet}`);
+
+    var options: HubSendToConnectionOptions = {};
+    options["contentType"] = typeof packet == "string" ? "text/plain" : "application/octet-stream";
+    await this.serviceClient.sendToConnection(this.connectionId, packet, options);
+
+    cb && cb();
+  }
 }
---
+++ experimental/sdk/webpubsub-socketio-extension/src/EIO/components/constants.ts
@@ -0,0 +1,2 @@
+export const EIO_SERVER_ACCEPT_CONNECTION: string = "eio-server-accept-connection";
+export const EIO_SERVER_REFUSE_CONNECTION: string = "eio-server-refuse-connection";
