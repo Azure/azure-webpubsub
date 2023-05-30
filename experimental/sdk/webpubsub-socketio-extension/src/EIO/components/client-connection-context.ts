0b65252c331f6ffd1507e0c4e6a2a5bd65887e88
xingsy97 <87063252+xingsy97@users.noreply.github.com>
2023-05-30 19:31:53 +0800

    resolve comments

--- experimental/sdk/webpubsub-socketio-extension/src/EIO/components/client-connection-context.ts
+++ experimental/sdk/webpubsub-socketio-extension/src/EIO/components/client-connection-context.ts
@@ -1,6 +1,7 @@
 import { debugModule } from "../../common/utils";
-import { HubSendToConnectionOptions, WebPubSubServiceClient } from "@azure/web-pubsub";
-import EventEmitter from "events";
+import { HubSendToConnectionOptions, WebPubSubServiceClient, JSONTypes} from "@azure/web-pubsub";
+import { RequestBodyType } from '@azure/core-rest-pipeline';
+import { ConnectResponse as WebPubSubConnectResponse, ConnectResponseHandler as WebPubSubConnectResponseHandler } from "@azure/web-pubsub-express";
 
 const debug = debugModule("wps-sio-ext:EIO:ClientConnectionContext");
 
@@ -8,23 +9,55 @@ const debug = debugModule("wps-sio-ext:EIO:ClientConnectionContext");
  * A logical concept that stands for an Engine.IO client connection, every connection has a unique `connectionId`.
  * It maps Engine.IO Transport behaviours to Azure Web PubSub service REST API calls.
  */
-export class ClientConnectionContext extends EventEmitter {
+export class ClientConnectionContext {
   public serviceClient: WebPubSubServiceClient;
   public connectionId: string;
+  public connectResponded: boolean;
+  private _connectResponseHandler: WebPubSubConnectResponseHandler;
 
-  constructor(serviceClient: WebPubSubServiceClient, connectionId: string) {
-    super();
-    this.serviceClient = serviceClient;
-    this.connectionId = connectionId;
+  constructor(serviceClient: WebPubSubServiceClient, connectionId: string, connectResponseHandler: WebPubSubConnectResponseHandler) {
+    this.serviceClient = serviceClient ?? (() => { throw new Error("serviceClient cannot be null"); })();
+    this.connectionId = connectionId ?? (() => { throw new Error("serviceClient cannot be null"); })();
+    this._connectResponseHandler = connectResponseHandler?? (() => { throw new Error("connectResponseHandler cannot be null"); })();
+    this.connectResponded = false;
   }
 
-  public async send(packet: any, cb?: (err?: Error) => void) {
-    debug(`send packet ${packet}, type = ${typeof packet}`);
+  /**
+   * Send `message` to a the bound client connection.
+   * @param message The message
+   * @param cb Callback function to handle error
+   */
+  public async send(message: string | RequestBodyType | JSONTypes, cb?: (err?: Error) => void) {
+    debug(`send message ${message}, type = ${typeof message}`);
 
     var options: HubSendToConnectionOptions = {};
-    options["contentType"] = typeof packet == "string" ? "text/plain" : "application/octet-stream";
-    await this.serviceClient.sendToConnection(this.connectionId, packet, options);
+    options["contentType"] = typeof message === "string" ? "text/plain" : "application/octet-stream";
 
-    cb && cb();
+    try {
+      await this.serviceClient.sendToConnection(this.connectionId, message, options);
+    } catch (error) {
+      cb && cb(error);  
+    }
+  }
+
+  /**
+   * Action after an EIO connection is accepted by EIO server and the server is trying to send open packet to client
+   * @param openPacketPayload 
+   */
+  public onAcceptEioConnection(openPacketPayload: string) {
+    let payloadWithoutType = openPacketPayload.substring(1);
+    this._connectResponseHandler.success({
+      socketioHandshake: JSON.parse(payloadWithoutType),
+    } as WebPubSubConnectResponse); 
+    this.connectResponded = true;
+  }
+
+  /**
+   * Action after an EIO connection is refused by EIO server
+   * @param errorMessage 
+   */
+  public onRefuseEioConnection(errorMessage: string) {
+    this._connectResponseHandler.fail(400, `EIO server refused connection with error: ${errorMessage}`);
+    this.connectResponded = true;
   }
 }
--- experimental/sdk/webpubsub-socketio-extension/src/EIO/components/constants.ts
+++ experimental/sdk/webpubsub-socketio-extension/src/EIO/components/constants.ts
@@ -1,3 +1,1 @@
-export const EIO_SERVER_ACCEPT_CONNECTION: string = "eio-server-accept-connection";
-export const EIO_SERVER_REFUSE_CONNECTION: string = "eio-server-refuse-connection";
 export const WEBPUBSUB_TRANSPORT_NAME: string = "webpubsub";
