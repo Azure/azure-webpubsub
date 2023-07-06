9a91f631945b15c6d931d5a3f14e7e5c14a2424d
Siyuan Xing <87063252+xingsy97@users.noreply.github.com>
2023-07-06 14:15:10 +0800

    Add unit tests for Socket.IO (#512)

---
+++ experimental/sdk/webpubsub-socketio-extension/test/SIO/index.ts
@@ -0,0 +1,8 @@
+// Modified from https://github.com/socketio/socket.io/blob/4.6.1/test/index.ts
+
+"use strict";
+
+describe("socket.io", () => {
+  require("./socket");
+  require("./namespace");
+});
