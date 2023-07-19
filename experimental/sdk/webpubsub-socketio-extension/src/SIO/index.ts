aac70063a4024d25186e39b07549e9700518ae5d
Liangying Wei <lianwei@microsoft.com>
2023-07-19 17:11:27 +0800

    Reset server to service call to rest api

--- experimental/sdk/webpubsub-socketio-extension/src/SIO/index.ts
+++ experimental/sdk/webpubsub-socketio-extension/src/SIO/index.ts
@@ -42,9 +42,10 @@ export async function useAzureSocketIO(
   if (!useDefaultAdapter) {
     debug("use webPubSub adatper");
 
+    // TODO: change undefined to serverProxy to enable server to service side tunneling
     const adapterProxy = new WebPubSubAdapterProxy(
       (this.engine as WebPubSubEioServer).webPubSubConnectionManager.service,
-      serverProxy
+      undefined
     );
     this.adapter(adapterProxy as unknown as AdapterConstructor);
   }
