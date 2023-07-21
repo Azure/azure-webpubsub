78b23a68f361e5aaf233792652f0b93c38aee6ad
Chenyang Liu <chenyl@microsoft.com>
2023-07-21 14:52:12 +0800

    format

--- experimental/sdk/webpubsub-socketio-extension/src/common/rest-service-client.ts
+++ experimental/sdk/webpubsub-socketio-extension/src/common/rest-service-client.ts
@@ -5,7 +5,11 @@ import { WebPubSubServiceCaller } from "awps-tunnel-proxies";
  * Communicate with Azure Web PubSub service via traditional REST API.
  */
 export class RestServiceClient extends WebPubSubServiceClient implements WebPubSubServiceCaller {
-    invoke(message: string, body: (data: Uint8Array, end: boolean) => void, options?: { filter: string; contentType: string; }): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
+  invoke(
+    message: string,
+    body: (data: Uint8Array, end: boolean) => void,
+    options?: { filter: string; contentType: string }
+  ): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
 }
