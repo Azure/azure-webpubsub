e0a1e1f1dcc5f92059f6c83ac3ddfa59dcbf1ee0
Chenyang Liu <chenyl@microsoft.com>
2023-08-14 14:05:18 +0800

    Update

--- experimental/sdk/webpubsub-socketio-extension/src/SIO/components/web-pubsub-adapter.ts
+++ experimental/sdk/webpubsub-socketio-extension/src/SIO/components/web-pubsub-adapter.ts
@@ -360,7 +360,7 @@ opts = ${toOptionsString(opts)}, error = ${e}`);
   /**
    * Generates OData filter string for Web PubSub service from a set of rooms and a set of exceptions
    * @param rooms - a set of Rooms to include
-   * @param except - a set of Rooms to exclude
+   * @param excepts - a set of Rooms to exclude
    * @returns OData - filter string
    */
   private _buildODataFilter(rooms: Set<string>, excepts?: Set<string> | undefined): string {
@@ -394,13 +394,12 @@ opts = ${toOptionsString(opts)}, error = ${e}`);
   }
 
   /**
-   * Generates OData filter string for Web PubSub service from a set of rooms and a set of exceptions
-   * @param rooms - a set of Rooms to include
-   * @param except - a set of Rooms to exclude
+   * Generates OData filter string for Web PubSub service from a set of exceptions
+   * @param excepts - a set of Rooms to exclude
    * @returns OData - filter string
    */
   private _buildODataFilterForExceptsOnly(excepts?: Set<string> | undefined): string {
-    debug("_buildODataFilter");
+    debug("_buildODataFilterForExceptsOnly");
     let except_idx = 0;
 
     let denyFilter = "";
@@ -412,9 +411,8 @@ opts = ${toOptionsString(opts)}, error = ${e}`);
       }
     }
 
-    let result = denyFilter.length > 0 ? `${denyFilter}` : "";
-    debug(`_buildODataFilter result = ${result}`);
-    return result;
+    debug(`_buildODataFilterForExceptsOnly result = ${denyFilter}`);
+    return denyFilter;
   }
 
   private _getEioSid(sioSid: string): string {
