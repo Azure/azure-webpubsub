c862fd399087d0baf7a9459620e75da5406d0186
Liangying.Wei <lianwei@microsoft.com>
2023-08-17 10:29:13 +0800

    Fix the audience of the generated token (#592)
    * Fix the audience of the generated token
    
    * Fix lint

--- experimental/sdk/webpubsub-socketio-extension/src/common/rest-service-client.ts
+++ experimental/sdk/webpubsub-socketio-extension/src/common/rest-service-client.ts
@@ -1,7 +1,18 @@
-import { WebPubSubServiceClient } from "@azure/web-pubsub";
+import { WebPubSubServiceClient, GenerateClientTokenOptions, ClientTokenResponse } from "@azure/web-pubsub";
 import { WebPubSubServiceCaller } from "../serverProxies";
 import { getInvokeOperationSpec } from "./azure-api/operation-spec";
 import { debugModule } from "./utils";
+import { isTokenCredential } from "@azure/core-auth";
+import jwt from "jsonwebtoken";
+
+import { createTracingClient } from "@azure/core-tracing";
+
+/** @internal */
+const tracingClient = createTracingClient({
+  namespace: "Microsoft.WebPubSub",
+  packageName: "@azure/web-pubsub-socket.io",
+});
+
 import * as coreClient from "@azure/core-client";
 
 const debug = debugModule("wps-sio-ext:common:rest-service-client");
@@ -53,5 +64,42 @@ rawResponse = ${JSON.stringify(rawResponse)}`);
       operationArguments,
       getInvokeOperationSpec(this.endpoint)
     );
+  }
+
+  /**
+   * Generate a token for a client to connect to the Azure Web PubSub service.
+   *
+   * @param options - Additional options
+   */
+  public async getClientAccessToken(options: GenerateClientTokenOptions = {}): Promise<ClientTokenResponse> {
+    return tracingClient.withSpan("getClientAccessToken", options, async (updatedOptions) => {
+      const endpoint = this.endpoint.endsWith("/") ? this.endpoint : this.endpoint + "/";
+      const baseUrl = `${endpoint}clients/socketio/hubs/${this.hubName}`;
+      const credential = this["credential"];
+      const innerClient = this["client"];
+      let token: string;
+      if (isTokenCredential(credential)) {
+        const response = await innerClient.webPubSub.generateClientToken(this.hubName, updatedOptions);
+        token = response.token!;
+      } else {
+        const key = credential.key;
+        const payload = { role: options?.roles, "webpubsub.group": options?.groups };
+        const signOptions: jwt.SignOptions = {
+          audience: baseUrl,
+          expiresIn: options?.expirationTimeInMinutes === undefined ? "1h" : `${options.expirationTimeInMinutes}m`,
+          algorithm: "HS256",
+        };
+        if (options?.userId) {
+          signOptions.subject = options?.userId;
+        }
+        token = jwt.sign(payload, key, signOptions);
+      }
+
+      return {
+        token,
+        baseUrl,
+        url: `${baseUrl}?access_token=${token}`,
+      };
+    });
   }
 }
