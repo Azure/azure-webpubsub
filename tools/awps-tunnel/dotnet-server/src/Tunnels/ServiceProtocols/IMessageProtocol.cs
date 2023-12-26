06d2b326ffa71cdb8f3a7e7a194cee9f1ac0c6d6
Liangying Wei <lianwei@microsoft.com>
2023-12-26 14:27:35 +0800

    remove dotnet tool version

--- tools/awps-tunnel/dotnet-server/src/Tunnels/ServiceProtocols/IMessageProtocol.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Buffers;
-
-public interface IMessageProtocol<T>
-{
-    bool TryParse(ref ReadOnlySequence<byte> buffer, out T? message);
-    void Write(T message, IBufferWriter<byte> writer);
-}
