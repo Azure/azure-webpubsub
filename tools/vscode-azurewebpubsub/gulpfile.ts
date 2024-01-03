8209e69f3848fcd681ac46ef4c5f418f25ca58ec
xingsy97 <87063252+xingsy97@users.noreply.github.com>
2024-01-03 15:35:07 +0800

    add test, resolve workspace issue

--- tools/vscode-azurewebpubsub/gulpfile.ts
+++ tools/vscode-azurewebpubsub/gulpfile.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { gulp_webpack } from '@microsoft/vscode-azext-dev';
+import { gulp_installAzureAccount, gulp_installResourceGroups, gulp_webpack } from '@microsoft/vscode-azext-dev';
 import * as fse from 'fs-extra';
 import * as gulp from 'gulp';
 import * as path from 'path';
@@ -14,9 +14,32 @@ async function prepareForWebpack(): Promise<void> {
     const mainJsPath: string = path.join(__dirname, 'main.js');
     let contents: string = (await fse.readFile(mainJsPath)).toString();
     contents = contents
-        .replace('out/extension.bundle', 'dist/extension.bundle')
+        .replace('out/src/extension', 'dist/extension.bundle')
         .replace(', true /* ignoreBundle */', '');
     await fse.writeFile(mainJsPath, contents);
+}
+
+async function listIcons(): Promise<void> {
+    const rootPath: string = path.join(__dirname, 'resources', 'providers');
+    const subDirs: string[] = (await fse.readdir(rootPath)).filter(dir => dir.startsWith('microsoft.'));
+    // tslint:disable-next-line: no-constant-condition
+    while (true) {
+        const subDir: string | undefined = subDirs.pop();
+        if (!subDir) {
+            break;
+        } else {
+            const subDirPath: string = path.join(rootPath, subDir);
+            const paths: string[] = await fse.readdir(subDirPath);
+            for (const p of paths) {
+                const subPath: string = path.posix.join(subDir, p);
+                if (subPath.endsWith('.svg')) {
+                    console.log(`'${subPath.slice(0, -4)}',`);
+                } else {
+                    subDirs.push(subPath);
+                }
+            }
+        }
+    }
 }
 
 async function cleanReadme(): Promise<void> {
@@ -26,13 +49,8 @@ async function cleanReadme(): Promise<void> {
     await fse.writeFile(readmePath, data);
 }
 
-async function preTest(): Promise<void> {
-    const fromPath: string = path.join(__dirname, 'test', 'ingress', 'dockerfileSamples');
-    const toPath: string = path.join(__dirname, 'dist', 'test', 'ingress', 'dockerfileSamples');
-    fse.copySync(fromPath, toPath);
-}
-
 exports['webpack-dev'] = gulp.series(prepareForWebpack, () => gulp_webpack('development'));
 exports['webpack-prod'] = gulp.series(prepareForWebpack, () => gulp_webpack('production'));
+exports.preTest = gulp.series(gulp_installAzureAccount, gulp_installResourceGroups);
+exports.listIcons = listIcons;
 exports.cleanReadme = cleanReadme;
-exports.preTest = preTest;
