f61eba373cb037930830f9cafa312ec56c5c5dea
xingsy97 <87063252+xingsy97@users.noreply.github.com>
2024-01-04 12:34:53 +0800

    Add CreateServiceInPortal for VSC Extension

--- tools/vscode-azurewebpubsub/src/commands.ts
+++ tools/vscode-azurewebpubsub/src/commands.ts
@@ -3,6 +3,29 @@
 *  Licensed under the MIT License. See License.txt in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
 
+import { parseError, registerCommandWithTreeNodeUnwrapping, type CommandCallback, type IActionContext, type IParsedError } from "@microsoft/vscode-azext-utils";
+import { instrumentOperation } from "vscode-extension-telemetry-wrapper";
+import { showError } from "./utils";
+import { createServiceInPortal } from "./workflows/service/create/createServiceInPortal";
+
+
+function registerCommandWithTelemetryWrapper(commandId: string, callback: CommandCallback): void {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    const callbackWithTroubleshooting: CommandCallback = (context: IActionContext, ...args: []) => instrumentOperation(commandId, async () => {
+        try {
+            await callback(context, ...args);
+        } catch (error) {
+            const e: IParsedError = parseError(error);
+            if (!e.isUserCancelledError) {
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+                showError(commandId, error);
+            }
+            throw error;
+        }
+    })();
+    registerCommandWithTreeNodeUnwrapping(commandId, callbackWithTroubleshooting);
+}
 
 export function registerCommands(): void {
+    registerCommandWithTelemetryWrapper('azureWebPubSub.service.createInPortal', createServiceInPortal);
 }
