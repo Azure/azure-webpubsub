0aeb07fec9c35fad25c23fddeda3c20cf35fb74c
xingsy97 <87063252+xingsy97@users.noreply.github.com>
2024-02-21 19:15:03 +0800

    update

--- tools/vscode-azurewebpubsub/src/tree/service/ServiceItem.ts
+++ tools/vscode-azurewebpubsub/src/tree/service/ServiceItem.ts
@@ -6,7 +6,7 @@
 import { KnownServiceKind, WebPubSubResource } from "@azure/arm-webpubsub";
 import { getResourceGroupFromId, uiUtils } from "@microsoft/vscode-azext-azureutils";
 import { IActionContext, TreeElementBase, createContextValue } from "@microsoft/vscode-azext-utils";
-import { AzureResource, AzureSubscription } from '@microsoft/vscode-azureresources-api';
+import { AzureResource, AzureResourceModel, AzureSubscription, ViewPropertiesModel } from '@microsoft/vscode-azureresources-api';
 import * as vscode from 'vscode';
 import { createWebPubSubAPIClient } from "../../utils";
 import { getServiceIconPath } from "../utils";
@@ -14,8 +14,8 @@ import { ServicePropertiesItem } from "./ServicePropertiesItem";
 import { ServiceModel } from "./ServiceModel";
 
 
-export class ServiceItem implements TreeElementBase {
-    static readonly contextValue: string = 'serviceItem';
+export class ServiceItem implements AzureResourceModel {
+    static readonly contextValue: string = 'webPubSubServiceItem';
     static readonly contextValueRegExp: RegExp = new RegExp(ServiceItem.contextValue);
 
     id: string;
@@ -44,6 +44,11 @@ export class ServiceItem implements TreeElementBase {
             tooltip: `${this.service.name}, ${this.service.sku?.tier} ${this.service.sku?.capacity} units, ${this.service.location}`,
         };
     }
+
+    viewProperties: ViewPropertiesModel = {
+        data: this.service,
+        label: `Properties`
+    };
 
     static async List(context: IActionContext, subscription: AzureSubscription): Promise<WebPubSubResource[]> {
         const client = await createWebPubSubAPIClient(context, subscription);
