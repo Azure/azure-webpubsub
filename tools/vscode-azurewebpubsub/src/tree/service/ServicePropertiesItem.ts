d629d7c6164190c801db5e697b92f96e684d13c4
xingsy97 <87063252+xingsy97@users.noreply.github.com>
2024-02-22 11:16:03 +0800

    resolve comment

--- tools/vscode-azurewebpubsub/src/tree/service/ServicePropertiesItem.ts
+++ tools/vscode-azurewebpubsub/src/tree/service/ServicePropertiesItem.ts
@@ -25,18 +25,7 @@ export class ServicePropertiesItem implements TreeElementBase {
             collapsibleState: vscode.TreeItemCollapsibleState.Collapsed
         };
     }
-
-    private getIconPathForProvisoningState(provisioningState?: ProvisioningState): TreeItemIconPath {
-        switch (provisioningState) {
-            case "Running": case "Creating": case "Updating": case "Deleting": case "Moving": return new ThemeIcon("refresh");
-            case "Succeeded": return new ThemeIcon("check"); 
-            case "Failed": return new ThemeIcon("error"); 
-            case "Canceled": return new ThemeIcon("close"); 
-            case "Unknown": return new ThemeIcon("question"); 
-            default: return new ThemeIcon("error"); 
-        }
-    }
-
+    
     async getChildren(): Promise<TreeElementBase[]> {
         const childs: TreeElementBase[] = [];
         childs.push(createGenericElement({
@@ -46,45 +35,37 @@ export class ServicePropertiesItem implements TreeElementBase {
             iconPath: new ThemeIcon("location")
         }));
         childs.push(createGenericElement({
-            label: localize('serviceProvisioningState', 'Provisioning State'),
+            label: localize('serviceSku', 'Sku'),
+            description: this.webPubSub.sku?.name,
+            contextValue: "serviceSku",
+            iconPath: new ThemeIcon("plug")
+        }));
+        childs.push(createGenericElement({
+            label: localize('serviceUnitCount', 'Unit'),
+            description: (this.webPubSub.sku?.capacity ?? 1).toString(),
+            contextValue: "serviceUnitCount",
+            iconPath: new ThemeIcon("symbol-unit")
+        }));
+        childs.push(createGenericElement({
+            label: localize('serviceStatus', 'Status'),
             description: this.webPubSub.provisioningState,
             contextValue: "serviceStatus",
             iconPath: this.getIconPathForProvisoningState(this.webPubSub.provisioningState)
         }));
-        childs.push(createGenericElement({
-            label: localize('serviceLocalAuth', 'Local Auth'),
-            description: this.webPubSub.disableLocalAuth ? "Disabled" : "Enabled",
-            contextValue: "servicePropertyDisableLocalAuth",
-            iconPath: new ThemeIcon(this.webPubSub.disableLocalAuth ? "error" : "check"),
-        }));
-        childs.push(createGenericElement({
-            label: localize('servicePublicNetworkAccess', 'Public Network Access'),
-            description: this.webPubSub.publicNetworkAccess ? "Allow" : "Deny",
-            contextValue: "servicePropertyPublicNetworkAccess",
-            iconPath: new ThemeIcon(this.webPubSub.publicNetworkAccess ? "check" : "error"),
-        }));
 
-        let tlsNodeDesc = "";
-        if (this.webPubSub.sku?.tier !== KnownWebPubSubSkuTier.Free) {
-            if (this.webPubSub.tls) {
-                tlsNodeDesc = this.webPubSub.tls.clientCertEnabled ? "Client Cert Enabled" : "Client Cert Disabled";
-            }
-            else {
-                tlsNodeDesc = "Unconfigured";
-            }
-        }
-        else {
-            tlsNodeDesc = `Not Supported for ${this.webPubSub.sku?.tier} Tier`;
-        }
-
-        childs.push(createGenericElement({
-            label: localize('serviceTls', 'TLS'),
-            description: tlsNodeDesc,
-            contextValue: "serviceTls",
-            iconPath: new ThemeIcon(tlsNodeDesc.includes("Enabled") ? "check" : "error"),
-        }));
-        
         return childs;
+    }
+
+    
+    getIconPathForProvisoningState(provisioningState?: ProvisioningState): TreeItemIconPath {
+        switch (provisioningState) {
+            case "Running": case "Creating": case "Updating": case "Deleting": case "Moving": return new ThemeIcon("refresh");
+            case "Succeeded": return new ThemeIcon("check"); 
+            case "Failed": return new ThemeIcon("error"); 
+            case "Canceled": return new ThemeIcon("close"); 
+            case "Unknown": return new ThemeIcon("question"); 
+            default: return new ThemeIcon("error"); 
+        }
     }
 
 }
