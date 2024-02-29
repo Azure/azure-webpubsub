bb327d51bf1e048cabe6dde3e309228d9b420f07
Siyuan <87063252+xingsy97@users.noreply.github.com>
2024-02-29 16:59:48 +0800

    Update getKeyTypeStep.ts

--- tools/vscode-azurewebpubsub/src/workflows/common/getKeyTypeStep.ts
+++ tools/vscode-azurewebpubsub/src/workflows/common/getKeyTypeStep.ts
@@ -12,11 +12,7 @@ const keyTypePickItems: IAzureQuickPickItem<KeyType>[] = [
 
 export class GetKeyTypeStep extends AzureWizardPromptStep<IPickKeyContext> {
     public async prompt(context: IPickKeyContext): Promise<void> {
-        const chosenItem = await context.ui.showQuickPick(keyTypePickItems, {
-            placeHolder: localize("key", `Select key type`),
-            suppressPersistence: true,
-        });
-        context.keyType = chosenItem.data;
+        context.keyType = (await context.ui.showQuickPick(keyTypePickItems, { placeHolder: localize("key", `Select key type`)})).data;
     }
 
     public shouldPrompt(_context: IPickKeyContext): boolean { return true; }
