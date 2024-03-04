d37436550528862d54cf54864be74b621f2ae8e6
Siyuan <87063252+xingsy97@users.noreply.github.com>
2024-03-04 14:06:56 +0800

    Support copy endpoint in VSCode extension (#693)

--- tools/vscode-azurewebpubsub/src/commands.ts
+++ tools/vscode-azurewebpubsub/src/commands.ts
@@ -9,8 +9,9 @@ import { showError } from "./utils";
 import { createServiceInPortal } from "./workflows/service/create/createServiceInPortal";
 import { createServiceForClassical, createServiceForSocketIO } from "./workflows/service/create/createService";
 import { restartService } from "./workflows/service/restart/restartService";
-import { copyConnectionString } from "./workflows/service/copyConnectionString/copyConnectionString";
 import { deleteService } from "./workflows/service/delete/deleteService";
+import { copyServiceEndpoint } from "./workflows/service/copyEndpoint/copyEndpoint";
+import { copyConnectionString } from "./workflows/service/copyConnectionString/copyConnectionString";
 
 function registerCommandWithTelemetryWrapper(commandId: string, callback: CommandCallback): void {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
@@ -37,4 +38,5 @@ export function registerCommands(): void {
     registerCommandWithTelemetryWrapper('azureWebPubSub.service.delete', deleteService);
     registerCommandWithTelemetryWrapper('azureWebPubSub.service.restart', restartService);
     registerCommandWithTelemetryWrapper('azureWebPubSub.service.copyConnectionString', copyConnectionString);
+    registerCommandWithTelemetryWrapper('azureWebPubSub.service.copyEndpoint', copyServiceEndpoint);
 }
--- tools/vscode-azurewebpubsub/src/utils.ts
+++ tools/vscode-azurewebpubsub/src/utils.ts
@@ -29,3 +29,7 @@ export async function createActivityContext(): Promise<ExecuteActivityContext> {
 export function showError(commandName: string, error: Error): void {
     void window.showErrorMessage(`Command "${commandName}" fails. ${error.message}`);
 }
+
+export function createEndpointFromHostName(hostName: string): string {
+    return `https://${hostName}`;
+}
