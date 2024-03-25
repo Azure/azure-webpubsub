0d7ee8374d16843ae1a92c3c40f6c29a34fee164
xingsy97 <87063252+xingsy97@users.noreply.github.com>
2024-03-25 12:11:10 +0800

    Support updating single event handler

---
+++ tools/vscode-azurewebpubsub/src/workflows/hubSetting/EventHandler/update/updateEventHandler.ts
@@ -0,0 +1,48 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.txt in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+
+import { WebPubSubManagementClient } from "@azure/arm-webpubsub";
+import { createAzureClient } from "@microsoft/vscode-azext-azureutils";
+import { type IActionContext} from "@microsoft/vscode-azext-utils";
+import { AzureWizard, createSubscriptionContext } from "@microsoft/vscode-azext-utils";
+import { ext } from "../../../../extensionVariables";
+import { createActivityContext, localize } from "../../../../utils";
+import { CreateOrUpdateEventHandlerStep } from "../../common/CreateOrUpdateEventHandlerStep";
+import { CreateOrUpdateHubSettingStep } from "../../common/CreateOrUpdateHubSettingStep";
+import { type ICreateOrUpdateHubSettingContext } from "../../../common/contexts";
+import { type EventHandlerItem } from "../../../../tree/hubSettings/EventHandlers/EventHandlerItem";
+import { pickEventHandler } from "../../../../tree/hubSettings/EventHandlers/pickEventHandler";
+
+export async function updateEventHandler(context: IActionContext, node?: EventHandlerItem): Promise<void> {
+    const parentHubSetting = (node ?? await pickEventHandler(context)).eventHandlersItem.hubItem;
+    if (!parentHubSetting) {
+        throw new Error(localize('invalidParentHubSetting', 'Invalid Parent Hub Setting'));
+    }
+
+    const serivce = parentHubSetting.service;
+    const subContext = createSubscriptionContext(serivce.subscription);
+    
+    const wizardContext: ICreateOrUpdateHubSettingContext = {
+        ...context,
+        ...await createActivityContext(),
+        subscription: subContext,
+        resourceGroupName: serivce.resourceGroup,
+        serviceName: serivce.name,
+        hubName: parentHubSetting.hub.hubName,
+        hubProperties: parentHubSetting.hub.properties
+    };
+
+    const client = createAzureClient([context, subContext], WebPubSubManagementClient);
+
+    const wizard: AzureWizard<ICreateOrUpdateHubSettingContext> = new AzureWizard(wizardContext, {
+        title: localize('updateEventHandler', `Update event handler in hub setting ${parentHubSetting.hub.hubName}`),
+        promptSteps: [ new CreateOrUpdateEventHandlerStep(false) ],
+        executeSteps: [ new CreateOrUpdateHubSettingStep(client, false) ]
+    });
+
+    await wizard.prompt();
+    await wizard.execute();
+    ext.branchDataProvider.refresh();
+}
