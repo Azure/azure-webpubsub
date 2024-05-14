13b6da2eaedd01b9fbab4c8c116856af47ee6fb6
Yuxuan Jiang <jiangy10@rose-hulman.edu>
2024-05-14 13:52:58 +0800

    server panel updated with tab selection

--- tools/awps-tunnel/client/src/panels/ServerPanel.tsx
+++ tools/awps-tunnel/client/src/panels/ServerPanel.tsx
@@ -1,5 +1,5 @@
 import {Icon} from "@fluentui/react/lib/Icon";
-import {Switch, Field, ProgressBar} from "@fluentui/react-components";
+import {Switch, Field, ProgressBar, TabList, Tab} from "@fluentui/react-components";
 import restapiSpec from '../components/api/restapiSample.json'
 import {useEffect, useState} from "react";
 import {ConnectionStatus, PathItem, RESTApi} from "../models";
@@ -7,6 +7,7 @@ import CodeTabs from "../components/CodeTabs";
 import {useDataContext} from "../providers/DataContext";
 import {Path} from "../components/api/Path";
 import {EndpointNav} from "../components/api/EndpointNav";
+import type {SelectTabData, TabValue} from "@fluentui/react-components";
 
 export interface ServerPanelProps {
 	endpoint?: string;
@@ -22,6 +23,7 @@ export function ServerPanel({endpoint, onChange}: ServerPanelProps) {
 	const [selectedPath, setSelectedPath] = useState<string>();
 	const [pathUrl, setPathUrl] = useState<string>("");
 	const [method, setMethod] = useState<string>("");
+	const [selectedPanel, setSelectedPanel] = useState<TabValue>("")
 	useEffect(() => {
 		setStartEmbeddedServer(data.builtinUpstreamServerStarted);
 	}, [data.builtinUpstreamServerStarted]);
@@ -67,33 +69,45 @@ export function ServerPanel({endpoint, onChange}: ServerPanelProps) {
 	}
 	
 	return (
-		<div className="d-flex align-items-stretch m-2">
-			{/*<p>*/}
-			{/*  <Icon className="mx-2" iconName="ServerEnviroment"></Icon>*/}
-			{/*  <b>*/}
-			{/*    Requests are sending to*/}
-			{/*    {startEmbeddedServer ? " built-in Echo Server." : ` your local server: ${endpoint}`}*/}
-			{/*  </b>*/}
-			{/*</p>*/}
-			{/*<Switch*/}
-			{/*  label={startEmbeddedServer ? "Built-in Echo Server started" : "Built-in Echo Server stopped"}*/}
-			{/*  checked={startEmbeddedServer}*/}
-			{/*  disabled={status === ConnectionStatus.Connecting || status === ConnectionStatus.Disconnecting}*/}
-			{/*  onChange={(ev) => onSwitch(ev.currentTarget.checked)}*/}
-			{/*></Switch>*/}
-			{/*{(status === ConnectionStatus.Connecting || status === ConnectionStatus.Disconnecting) && (*/}
-			{/*  <Field className="m-2" validationMessage={status === ConnectionStatus.Connecting ? "Starting built-in Echo Server" : "Stopping built-in Echo Server"} validationState="none">*/}
-			{/*    <ProgressBar />*/}
-			{/*  </Field>*/}
-			{/*)}*/}
-			{/*<div className="m-2">*/}
-			{/*  <b>{message}</b>*/}
-			{/*  <hr></hr>*/}
-			{/*  <b>📋Sample code handling events in your app server:</b>*/}
-			{/*  <CodeTabs></CodeTabs>*/}
-			{/*</div>*/}
-				<EndpointNav setSelectedPath={setSelectedPath}></EndpointNav>
+		<div>
+			<TabList className="mb-2" selectedValue={selectedPanel} onTabSelect={(e, data) => {
+				setSelectedPanel(data.value)
+			}}>
+				<Tab id={"server"} value={"server"}>Server</Tab>
+				<Tab id={"api"} value={"api"}>REST API</Tab>
+			</TabList>
+			{selectedPanel === "server" && <div>
+		  <p>
+			  <Icon className="mx-2" iconName="ServerEnviroment"></Icon>
+			  <b>
+				  Requests are sending to
+								{startEmbeddedServer ? " built-in Echo Server." : ` your local server: ${endpoint}`}
+			  </b>
+		  </p>
+		  <Switch
+			  label={startEmbeddedServer ? "Built-in Echo Server started" : "Built-in Echo Server stopped"}
+			  checked={startEmbeddedServer}
+			  disabled={status === ConnectionStatus.Connecting || status === ConnectionStatus.Disconnecting}
+			  onChange={(ev) => onSwitch(ev.currentTarget.checked)}
+		  ></Switch>
+				{(status === ConnectionStatus.Connecting || status === ConnectionStatus.Disconnecting) && (
+					<Field className="m-2"
+					       validationMessage={status === ConnectionStatus.Connecting ? "Starting built-in Echo Server" : "Stopping built-in Echo Server"}
+					       validationState="none">
+						<ProgressBar/>
+					</Field>
+				)}
+		  <div className="m-2">
+			  <b>{message}</b>
+			  <hr></hr>
+			  <b>📋Sample code handling events in your app server:</b>
+			  <CodeTabs></CodeTabs>
+		  </div>
+	  </div>}
+			{selectedPanel === "api" && <div className="d-flex align-items-stretch m-2">
+		  <EndpointNav setSelectedPath={setSelectedPath}></EndpointNav>
 				{pathUrl && <Path pathItem={restAPI.paths[pathUrl]} path={pathUrl} methodName={method}/>}
+	  </div>}
 		</div>
 	);
 }
