dcdd57102b8806f633a8573e5dc1dec1ef91c527
Liangying Wei <lianwei@microsoft.com>
2024-06-13 11:02:09 +0800

    Adding integration tests

---
+++ tests/integration-tests/csharp/TestEnvironment.cs
@@ -0,0 +1,27 @@
+﻿
+using dotenv.net;
+
+namespace csharp
+{
+    [SetUpFixture]
+    public class TestEnvironment
+    {
+        public static string ConnectionString { get; private set; }
+
+        [OneTimeSetUp]
+        public void GlobalSetup()
+        {
+            // TODO: provision the resources?
+            // What if multiple different resources needed?
+            DotEnv.Load();
+
+            ConnectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING") ?? throw new InvalidOperationException("Please set the WEB_PUBSUB_CONNECTION_STRING environment variable.");
+        }
+
+        [OneTimeTearDown]
+        public void GlobalTeardown()
+        {
+            // Clean up any resources if necessary
+        }
+    }
+}
