91de7400f251857a7cf3c8846e8b6582e4cbea9b
Siyuan <87063252+xingsy97@users.noreply.github.com>
2024-07-02 14:54:49 +0800

    Add Socket.IO Admin UI (#748)

---
+++ tools/azure-socketio-admin-ui/CHANGELOG.md
@@ -0,0 +1,8 @@
+# Change Log
+
+All notable changes to this project will be documented in this file.
+
+## [0.0.1] - 2024-07-02
+
+- Based on [socket.io-admin-ui@0.5.1](https://github.com/socketio/socket.io-admin-ui/tree/0.5.1)
+- Added simple benchmark
---
+++ tools/azure-socketio-admin-ui/src/store/modules/servers.js
@@ -0,0 +1,49 @@
+import { find, merge } from "lodash-es";
+import { remove } from "../../util";
+
+const HEALTHY_THRESHOLD = 10000;
+
+export default {
+  namespaced: true,
+  state: {
+    servers: [],
+  },
+  getters: {
+    namespaces(state) {
+      const namespaces = {};
+      for (const server of state.servers) {
+        if (server.namespaces) {
+          for (const { name, socketsCount } of server.namespaces) {
+            namespaces[name] = (namespaces[name] || 0) + socketsCount;
+          }
+        }
+      }
+      return Object.keys(namespaces).map((name) => {
+        return {
+          name,
+          socketsCount: namespaces[name],
+        };
+      });
+    },
+  },
+  mutations: {
+    onServerStats(state, stats) {
+      stats.lastPing = Date.now();
+      const server = find(state.servers, { serverId: stats.serverId, pid: stats.pid });
+      if (server) {
+        merge(server, stats);
+      } else {
+        stats.healthy = true;
+        state.servers.push(stats);
+      }
+    },
+    removeServer(state, serverId) {
+      remove(state.servers, { serverId });
+    },
+    updateState(state) {
+      state.servers.forEach((server) => {
+        server.healthy = Date.now() - server.lastPing < HEALTHY_THRESHOLD;
+      });
+    },
+  },
+};
