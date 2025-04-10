72223761d020df719a72982f8a2013b2218421f4
cqnguy23 <44353219+cqnguy23@users.noreply.github.com>
2025-04-10 20:01:59 +0700

    Refactor C# protobuf client  (#841)
    * refactor directory
    
    * add readme and code refactor
    
    * edit workflow
    
    * fix warnings
    
    * simplify README

--- sdk/clients/csharp/protobuf-client/src/README.md
+++
@@ -1,5 +0,0 @@
-# Azure Web PubSub protobuf protocol client for .NET
-
-[Web PubSub](https://aka.ms/awps/doc) is an Azure-managed service that helps developers easily build web applications with real-time features and publish-subscribe patterns. Any scenario that requires real-time publish-subscribe messaging between server and clients or among clients can use Web PubSub. Traditional real-time features that often require polling from the server or submitting HTTP requests can also use Web PubSub.
-
-You can use this library to add protobuf subprotocols including `protobuf.reliable.webpubsub.azure.v1` and `protobuf.webpubsub.azure.v1` support to the Azure.Messaging.WebPubSub.Client library.
---
+++ sdk/clients/protobuf-client/csharp/tests/WebPubSubProtobufProtocolTests.cs
@@ -0,0 +1,40 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Buffers;
+using Azure.Core;
+using Azure.Messaging.WebPubSub.Clients;
+using Xunit;
+
+namespace Azure.Messaging.WebPubSub.Client.Protobuf.Tests;
+
+public class WebPubSubProtobufProtocolTests
+{
+    [Fact]
+    public void VerifyProtocolProperties()
+    {
+        // Arrange
+        var protocol = new WebPubSubProtobufProtocol();
+
+        // Assert
+        Assert.Equal("protobuf.webpubsub.azure.v1", protocol.Name);
+        Assert.Equal(WebPubSubProtocolMessageType.Binary, protocol.WebSocketMessageType);
+        Assert.False(protocol.IsReliable);
+    }
+
+    [Fact]
+    public void WriteMessageToBufferWriter()
+    {
+        // Arrange
+        var protocol = new WebPubSubProtobufProtocol();
+        var message = new JoinGroupMessage("testGroup", 123);
+        var writer = new ArrayBufferWriter<byte>();
+
+        // Act
+        protocol.WriteMessage(message, writer);
+
+        // Assert
+        Assert.True(writer.WrittenCount > 0);
+    }
+}
