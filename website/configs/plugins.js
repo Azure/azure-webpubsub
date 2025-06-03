8f2e7d72d41bfebab47ba7eb1525af5f24f8c86a
ruimhu1201 <ruimhu@microsoft.com>
2025-06-03 10:39:02 +0800

    remove GA

--- website/configs/plugins.js
+++ website/configs/plugins.js
@@ -1,28 +1,28 @@
 const path = require('path')
 const setupPlugins = function ({ root }) {
-  const plugins = [
-    async function tailwindPlugin(context, options) {
-      return {
-        name: 'docusaurus-tailwindcss',
-        configurePostCss(postcssOptions) {
-          // Appends TailwindCSS and AutoPrefixer.
-          postcssOptions.plugins.push(require('tailwindcss'))
-          postcssOptions.plugins.push(require('autoprefixer'))
-          return postcssOptions
+    const plugins = [
+        async function tailwindPlugin(context, options) {
+            return {
+                name: 'docusaurus-tailwindcss',
+                configurePostCss(postcssOptions) {
+                    // Appends TailwindCSS and AutoPrefixer.
+                    postcssOptions.plugins.push(require('tailwindcss'))
+                    postcssOptions.plugins.push(require('autoprefixer'))
+                    return postcssOptions
+                },
+            }
         },
-      }
-    },
 
-    // Handle "back to main menu" issue with Docusaurus
-    // When the rendered webpage zoomed to 200%, the "back to main menu" is not functional.
-    // This function removes the DOM element after rendering.
-    function handleBackToMainMenuBtn(context, options) {
-      return {
-        name: 'handle-back-to-main-btn',
-        injectHtmlTags({ content }) {
-          return {
-            postBodyTags: [
-              `<script>
+        // Handle "back to main menu" issue with Docusaurus
+        // When the rendered webpage zoomed to 200%, the "back to main menu" is not functional.
+        // This function removes the DOM element after rendering.
+        function handleBackToMainMenuBtn(context, options) {
+            return {
+                name: 'handle-back-to-main-btn',
+                injectHtmlTags({ content }) {
+                    return {
+                        postBodyTags: [
+                            `<script>
                 document.addEventListener('keyup', (event)=> {
                   if(event.key === 'Tab') {
                     const backBtn = document.querySelector(".navbar-sidebar__back");
@@ -40,33 +40,14 @@ const setupPlugins = function ({ root }) {
                   if(backBtn) backBtn.style.display = "none";
                 });
             </script>`,
-            ],
-          }
+                        ],
+                    }
+                },
+            }
         },
-      }
-    },
-  ]
-
-
-  // only add gtag in production
-  const trackingID = process.env.trackingID
-  const gtmTrackingID = process.env.gtmTrackingID
-  if (trackingID && trackingID !== '' && gtmTrackingID && gtmTrackingID !== '') {
-    const gtagPlugin = [
-      path.resolve(root, 'src/plugins/visitor-tracking-plugin'),
-      {
-        trackingID,
-        gtmTrackingID,
-        anonymizeIP: true,
-      },
     ]
-    plugins.push(gtagPlugin)
-    console.log(`Add visitor-tracking-plugin Successfully. tracking ID: ${trackingID}, GTM tracking ID: ${gtmTrackingID}`)
-  } else {
-    console.warn('Failed to get "trackingID" for gtag! Should be null in development, but should be non empty string in Production.')
-  }
 
-  return plugins
+    return plugins
 }
 
 module.exports = setupPlugins
